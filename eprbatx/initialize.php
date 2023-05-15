001ef7f9827141dc3da7a71a1ad19282d4d8223b
BatxIT <133664309+BatxIT@users.noreply.github.com>
2023-05-15 19:32:46 +0530

    Update initialize.php
    Bluehost IP

--- eprbatx/initialize.php
+++ eprbatx/initialize.php
@@ -1,10 +1,10 @@
 <?php
 // $dev_data = array('id'=>'-1','firstname'=>'Developer','lastname'=>'','username'=>'dev_oretnom','password'=>'5da283a2d990e8d8512cf967df5bc0d0','last_login'=>'','date_updated'=>'','date_added'=>'');
-if(!defined('base_url')) define('base_url','http://3.7.208.89/eprbatx/');
+if(!defined('base_url')) define('base_url','162.214.81.14/epr');
 if(!defined('base_app')) define('base_app', str_replace('\\','/',__DIR__).'/' );
 // if(!defined('dev_data')) define('dev_data',$dev_data);
-if(!defined('DB_SERVER')) define('DB_SERVER',"3.7.208.89");
-if(!defined('DB_USERNAME')) define('DB_USERNAME',"root");
-if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"root");
-if(!defined('DB_NAME')) define('DB_NAME',"sts_db");
+if(!defined('DB_SERVER')) define('DB_SERVER',"localhost");
+if(!defined('DB_USERNAME')) define('DB_USERNAME',"pnbakmmy_epr_user");
+if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"Batx@IT11#");
+if(!defined('DB_NAME')) define('DB_NAME',"pnbakmmy_epr_db");
 ?>
